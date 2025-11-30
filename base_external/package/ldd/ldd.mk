4262894e537c0f581b2a02be9d98f3b385915cd1
rett84 <lucasrettore31@hotmail.com>
2025-11-30 14:44:26 -0500

    Update ldd.mk

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 7 git contents
-LDD_VERSION = 41335250121418f3930e2334294a4851908351b4
+LDD_VERSION = 90d49a1a65fcb5a8042640acbdf2bbf7790206f9
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
